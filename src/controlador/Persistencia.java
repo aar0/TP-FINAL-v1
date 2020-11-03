7686b1d5b35326f5d37c75e466ce51cb4ca08c24
aar0 <69370215+aar0@users.noreply.github.com>
2020-11-03 03:42:06 -0300

    v3

--- src/controlador/Persistencia.java
+++ src/controlador/Persistencia.java
@@ -1,5 +1,40 @@
 package controlador;
 
-public class Persistencia {
+import com.mysql.cj.xdevapi.Session;
+import com.mysql.cj.xdevapi.SessionFactory;
 
+public class Persistencia {
+	private final String configuracion = "HIBERNATE/hibernate.cfg.xml";
+	private SessionFactory factory;
+	private Session sesion;
+
+	public Persistencia() {
+		this.crearConexion();
+	}
+	protected void desconectar() {
+		/*
+		 * Primero se cierra la sesión.
+		 */
+		this.cerrarSesion();
+
+		/*
+		 * Se cierra la conexión a la base de datos desde el sessionFactory.
+		 */
+		if (this.factory != null) {
+			if (this.factory.isOpen()) {
+				this.factory.close();
+			}
+		}
+	}
+	protected void cerrarSesion() {
+		if (this.sesion != null) {
+			if (this.sesion.isConnected()) {
+				this.sesion.disconnect();
+			}
+
+			if (this.sesion.isOpen()) {
+				this.sesion.close();
+			}
+		}
+	}
 }
--- src/controlador/controlador.java
+++ src/controlador/controlador.java
@@ -2,10 +2,15 @@ package controlador;
 
 import javax.swing.JOptionPane;
 
+import util.NotificacionError;
+
 public class Controlador {
 	private Persistencia persistencia;
 	public Controlador(Persistencia persistencia) {
 		this.persistencia = persistencia;
+	}
+	public void desconectarBaseDatos() {
+		this.persistencia.desconectar();
 	}
 	public void agregar() {
 		JOptionPane.showMessageDialog(null, "error, RELLENE CAMPOS NECESARIOS!", "OK", 3);
