61fe135a041002ec811c58f36502c400a33264b5
aar0 <69370215+aar0@users.noreply.github.com>
2020-11-02 23:13:47 -0300

    v2

--- src/controlador/controlador.java
+++ src/controlador/controlador.java
@@ -3,6 +3,10 @@ package controlador;
 import javax.swing.JOptionPane;
 
 public class Controlador {
+	private Persistencia persistencia;
+	public Controlador(Persistencia persistencia) {
+		this.persistencia = persistencia;
+	}
 	public void agregar() {
 		JOptionPane.showMessageDialog(null, "error, RELLENE CAMPOS NECESARIOS!", "OK", 3);
 	}
--- src/controlador/main.java
+++ src/controlador/main.java
@@ -2,15 +2,59 @@ package controlador;
 
 import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
+
 import vista.VentanaPrincipal;
-import controlador.controlador;
+import controlador.*;
+
 public class main {
 
 	public static void main(String[] args) {
+		Persistencia persistencia = null;
+		Controlador controlador = null;
+
+		try {
+
+			/*
+			 * Se declara y crea una instancia de la Persistencia.
+			 */
+			persistencia = new Persistencia();
+
+			/*
+			 * Se declara y crea una instancia del Controlador.
+			 */
+			controlador = new Controlador(persistencia);
+
+			/*
+			 * Se declara y crea una instancia de la ventana principal, a la cual se le pasa
+			 * la instancia del Controlador.
+			 */
+			VentanaPrincipal ventana = new VentanaPrincipal(controlador);
+
+			/*
+			 * Se le indica a la ventana que se haga visible.
+			 */
+			ventana.setVisible(true);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+
+			/*
+			 * Se asegura de que se cierre la conexión de la base de datos.
+			 */
+			if (controlador != null) {
+				try {
+					controlador.desconectarBaseDatos();
+				} catch (NotificacionError e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					controlador cont = new controlador();
+					Controlador cont = new Controlador();
 					VentanaPrincipal frame = new VentanaPrincipal(cont);
 					frame.setVisible(true);
 				} catch (Exception e) {
